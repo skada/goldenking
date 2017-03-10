516d886807469d8c92243b66e709dbd25fd5eab7
Kout <kout.tk@gmail.com>
2017-03-10 22:22:28 +0100

    Set up Gulp

---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+var gulp = require('gulp');
+
+var autoprefixer = require('autoprefixer');
+var browserSync = require('browser-sync').create();
+var clean = require('gulp-clean');
+var postcss = require('gulp-postcss');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var watch = require('gulp-watch');
+var exec = require('child_process').exec;
+
+gulp.task('runserver', function(){
+	var proc = exec('python3 manage.py runserver', function (err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+  });
+});
+
+gulp.task('cssclean', function(){
+	return gulp.src('donations/static/*.css', { read: false }).pipe(clean());
+});
+
+gulp.task('csscompile', ['cssclean'], function(){
+	return gulp
+		.src('src/index.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(sourcemaps.init())
+		.pipe(postcss([autoprefixer({
+			// browsers: ['last 2 versions, >.8%, IE8']
+		})]))
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest('donations/static'))
+		.pipe(browserSync.stream());
+});
+
+gulp.task('develop', ['csscompile', 'runserver'], function(){
+	browserSync.init({
+		proxy:  {
+			target: 'http://127.0.0.1:8000',
+		},
+		open: false,
+		files: [
+			'**/*.html',
+			'**/*.js'
+		]
+	});
+	gulp.watch('src/*.scss', ['csscompile']);
+});
+
+gulp.task('default', ['develop']);
