41636a00e6c9070b31061b33d92ab4d354a7a79d
Kout <kout.tk@gmail.com>
2017-03-11 09:35:16 +0100

    Comment the bloody thing

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,11 @@ var sourcemaps = require('gulp-sourcemaps');
 var watch = require('gulp-watch');
 var exec = require('child_process').exec;
 
+// Jednotlivé úlohy
+// Volají se v příkazové řádce zapsáním `$ gulp nazevulohy`
+// Samotné `$ gulp` spustí výchozí úlohu s názvem default (viz níže)
+
+// spustí server
 gulp.task('runserver', function(){
 	var proc = exec('python3 manage.py runserver', function(err, stdout, stderr){
 		console.log(stdout);
@@ -16,27 +21,33 @@ gulp.task('runserver', function(){
 	});
 });
 
+// vymaže předchozí verzi CSS (úklid)
 gulp.task('cssclean', function(){
 	return gulp.src('donations/static/*.css', { read: false }).pipe(clean());
 });
 
+// zkompilujeme čerstvé CSS + vymaže předchozí verzi CSS (pomocí výše nastavené úlohy cssclean)
 gulp.task('csscompile', ['cssclean'], function(){
 	return gulp
-		.src('src/index.scss')
-		.pipe(sass().on('error', sass.logError))
-		.pipe(sourcemaps.init())
-		.pipe(postcss([autoprefixer({
+		.src('src/index.scss') // zdroj stylopisů
+		.pipe(sass().on('error', sass.logError)) // výpíše chybu, pokud kompilace selže
+		.pipe(sourcemaps.init()) // vytvoří sourcemap => užitečné při ladění v devTools
+		.pipe(postcss([autoprefixer({ // doplní prefixy pro prohlížeče, které to potřebují
 			// browsers: ['last 2 versions, >.8%, IE8']
 		})]))
 		.pipe(sourcemaps.write('.'))
-		.pipe(gulp.dest('donations/static'))
-		.pipe(browserSync.stream());
+		.pipe(gulp.dest('donations/static')) // sem zapíše výsledné CSS
+		.pipe(browserSync.stream()); // promítne změny CSS do stránky ‒ bez reloadu
 });
 
+// zavolá výše popsané úlohy:
+// 1. zkompiluje CSS
+// 2. spustí server na (v prohlížeči na adrese http://localhost:3000)
+// 3. zapne hlídače, který hlídá změny v souborech a při změně vykoná, co má
 gulp.task('develop', ['csscompile', 'runserver'], function(){
 	browserSync.init({
 		proxy: {
-			target: 'http://127.0.0.1:8000',
+			target: 'http://127.0.0.1:8000', // adresa spuštěné aplikace v Djangu
 		},
 		open: false,
 		files: [
@@ -44,7 +55,7 @@ gulp.task('develop', ['csscompile', 'runserver'], function(){
 			'**/*.js'
 		]
 	});
-	gulp.watch('src/*.scss', ['csscompile']);
+	gulp.watch('src/*.scss', ['csscompile']); // hlídač
 });
 
-gulp.task('default', ['develop']);
+gulp.task('default', ['develop']); // nastaví jako výchozí úlohu 'develop', tj. stačí zapsat gulp a spustí se
